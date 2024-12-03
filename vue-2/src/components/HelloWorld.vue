b9654483a004be00052a97875dd9f821de0ff4cc
ViavaCos <13811092694@163.com>
2024-12-03 08:21:38 +0000

    feat: 添加Element-UI

--- vue-2/src/components/HelloWorld.vue
+++ vue-2/src/components/HelloWorld.vue
@@ -27,12 +27,19 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+
+    <ElementTable />
   </div>
 </template>
 
 <script>
+import ElementTable from './ElementTable'
+
 export default {
   name: 'HelloWorld',
+  components: {
+    ElementTable
+  },
   props: {
     msg: String
   }
